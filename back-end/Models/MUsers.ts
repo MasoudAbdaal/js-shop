9feb8ee2e440316ccfcb0d17e72b1b87f8535eba
MasoudAbdaal <m_abdaal@outlook.com>
2022-03-23 12:55:18 +0430

    Fixed UTC timestamp, read/write file bug

--- back-end/Models/MUsers.ts
+++ back-end/Models/MUsers.ts
@@ -1,5 +1,6 @@
-import { existsSync, readFile, writeFile } from "fs";
+import { existsSync, readFile, readFileSync, writeFile } from "fs";
 import moment from "moment";
+import path from "path";
 import { v5 } from "uuid";
 
 import IUserEntity from "../Interfaces/IUserEntity";
@@ -9,25 +10,25 @@ class UsersList {
   FilePath: string;
 
   constructor() {
-    this.FilePath = "../UserData.json";
+    this.FilePath = path.join(__dirname, "../UserData.json");
     this.FileExist = existsSync(this.FilePath);
   }
 
   AddUser(newUser: IUserEntity) {
     newUser.ID = v5(newUser.UserName, "67ce8d95-6b86-416f-aea2-655f56825829");
-    newUser.CreationDate = moment(new Date(Date.now())).format(
-      "YYYY-MM-DD HH:MM:SS"
-    );
+    newUser.CreationDate = moment
+      .utc(new Date().toUTCString())
+      .format("YYYY-MM-DD H:m:s");
+
+    //TODO: Fix sync functions and properties which cause of exeptions
 
     if (this.FileExist) {
-      readFile(this.FilePath, (e, data) => {
-        if (e === null) {
-          const fileContent = JSON.parse(data.toString()) as Array<IUserEntity>;
-          fileContent.push(newUser);
-          this.WriteToFile(this.FilePath, fileContent);
-          return newUser;
-        } else return e;
-      });
+      const fileContent = JSON.parse(
+        readFileSync(this.FilePath, "utf8")
+      ) as Array<IUserEntity>;
+      fileContent.push(newUser);
+      this.WriteToFile(this.FilePath, fileContent);
+      return newUser;
     } else {
       this.WriteToFile(this.FilePath, [newUser]);
       return newUser;
@@ -44,9 +45,9 @@ class UsersList {
    * @param Content "Final element should be modified or something has been taken last changes"
    */
   private WriteToFile(Path: string, Content: Array<IUserEntity>) {
-    Content[Content.length - 1].ModificationDate = moment(
-      new Date(Date.now())
-    ).format("YYYY-MM-DD HH:MM:SS");
+    Content[Content.length - 1].ModificationDate = moment
+      .utc(new Date().toUTCString())
+      .format("YYYY-MM-DD H:m:s");
 
     writeFile(
       Path,
